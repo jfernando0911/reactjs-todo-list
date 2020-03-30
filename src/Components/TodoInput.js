e7a3694f174a3222cc4dbaea1bc856b54e4ffa4d
jfernando0911 <jfernando0911@gmail.com>
2020-03-30 04:42:24 -0600

    Now we can add and remove items from the todo list, and add them to another list of done tasks

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,39 +1,70 @@
 import React, { useState } from 'react';
 
+
 const TodoInput = () => {
-    const [inputValue, inputValues]=useState("");
-    const [element, setElements]=useState([]);
+    let [inputValue, inputValues] = useState("");
+    const [element, setElements] = useState([]);
+    const [doneTask, updateDoneTasks] = useState([]);
 
-    const Changing = (e) =>{
-        if(e){
+    const Changing = (e) => {
+        if (e) {
             inputValues(e.target.value);
-            
         }
 
-        
-    }
-    
-    const printValues = ()=>{
-        console.log("Funciona este botón");
-        console.log(inputValue);
+
     }
 
-    const pressedEnterKey= (e)=>{
-        // console.log(e.keyCode == 13);
-        
-        if(e.keyCode == 13){
-            setElements([...element, inputValue]); 
-            console.log(element);
+    const pressedEnterKey = (e) => {
+
+        if (e.keyCode === 13) {
+            setElements([...element, inputValue]);
+            inputValues("");
+            e.target.value = ""
         }
 
-        
-        // if ekeyCode === 13 console.log("Hello world");
+
+
+    }
+
+    const DeleteListItem = (e) => {
+
+
+        if (e.type === "mouseenter") {
+            console.log("We enter")
+            e.target.style.backgroundColor = "red";
+        } else if (e.type === "mouseleave") {
+            console.log("salimos")
+            e.target.style.backgroundColor = "";
+        }
+        else if (e.type === "click") {
+
+            let getArray = [...element];
+            let index = getArray.indexOf(e.target.textContent);
+            console.log(index);
+            if(index !== -1){
+                updateDoneTasks([...doneTask,getArray[index]]);
+                getArray.splice(index,1);
+                console.log(getArray);
+                // setElements([...element,getArray]);
+                setElements(getArray);
+                
+            }
+
+        }
     }
 
     return (
         <div>
-            <input type="text" onKeyDown={pressedEnterKey} onChange={Changing}placeholder="Type the task"/>
-            <button onClick={printValues}>Presionar</button>
+            <input type="text" onKeyDown={pressedEnterKey} onChange={Changing} placeholder="Type the task" />
+            <ul>
+                <h2>Task to do</h2>
+                {element.map((e, i) => <li key={i} onMouseEnter={DeleteListItem} onClick={DeleteListItem} onMouseLeave={DeleteListItem}>{e}</li>)}
+            </ul>
+            <ul>
+                <h2>TaskDone</h2>
+                {doneTask.map((e, i) => <li key={i}>{e}</li>)}
+            </ul>
+
         </div>
     );
 }
