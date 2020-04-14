1604e45475a4785746b9981b46a659ebfb989fb0
jfernando0911 <jfernando0911@gmail.com>
2020-04-14 03:18:13 -0500

    We can underline the list element when the checkbox is true, when false it does the opposite. Also, the animation of the trashIcon when the parent is hovered works now

--- src/Components/ListElement.js
+++ src/Components/ListElement.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import {createRef} from 'react';
 import styles from './listElementContainer.module.css';
 import i from '../Images/trash_icon.png';
 
 const ListElement = (props) => {
-    
+    const inputEl = createRef();
     console.log(props);
 
-    
+    const updateTextDecoration = (e) =>{
+        console.log("Checkbox value: ", e.target.checked);
+        console.log("This is my ref: ", inputEl.current);
+        if(e.target.checked === true){
+            inputEl.current.style.textDecoration = "line-through";
+        }else{
+            inputEl.current.style.textDecoration = "none";
+            
+        }
+    }
+    //Nos quedamoe en la linea 20, checked tenemos que mandar el valor para marcar la tarea
     return (
-        <div className={styles.listElementWrapper}      onClick={props.delete}  >
-            <li>
+        <li className={styles.listElementContainer}  onClick={props.delete} ref={inputEl}  >
+            <div className={styles.nameContainer} >
                 <div>{props.name}</div>
-                <img src={i} alt={"It's a trash icon"}/>
-            </li>
-
-
-        </div>
+            </div>
+            <div className={styles.deleteContainer}>
+                <img src={i} alt={"It's a trash icon"} />
+            </div>
+            <div className={styles.doneContainer}>
+                <input type="checkbox" onChange={updateTextDecoration} />
+            </div>
+        </li>
     );
 }
 
