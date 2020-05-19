c3a34363e63fef4ec433abf16c074dbcb65e4b7c
jfernando0911 <jfernando0911@gmail.com>
2020-05-19 13:07:00 -0500

    Updates

---
+++ precache-manifest.591e62bdd815b6204331e1957c9c92e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341f2f8aaf2d4c8e824f9ee6858c9700",
+    "url": "/reactjs-todo-list/index.html"
+  },
+  {
+    "revision": "f22b88909b831883f077",
+    "url": "/reactjs-todo-list/static/css/main.7ffb75a9.chunk.css"
+  },
+  {
+    "revision": "dd35a56a4f7f52208d36",
+    "url": "/reactjs-todo-list/static/js/2.ca8de035.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-todo-list/static/js/2.ca8de035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22b88909b831883f077",
+    "url": "/reactjs-todo-list/static/js/main.1de805f7.chunk.js"
+  },
+  {
+    "revision": "9dfd240f832c15d1f7c0",
+    "url": "/reactjs-todo-list/static/js/runtime-main.0e583aa1.js"
+  }
+]);
